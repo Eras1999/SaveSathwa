3458d0bc8d2b742a56b03b914c275225023dae36
Eranda Madusanka <madushankaeranda56@gmail.com>
2025-03-07 22:52:53 +0530

    slider js create

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        autoplay: {
+            delay: 7000, // Increased delay for a slower effect
+            disableOnInteraction: false,
+        },
+        effect: "fade",
+        fadeEffect: {
+            crossFade: true, // Ensures smooth fading between slides
+        },
+        speed: 2000, // Slows down the transition to 2 seconds
+       
+    });
+});
