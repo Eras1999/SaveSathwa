fc824fe3e5218a95bd33f0b86778ee85c0b3b075
Eranda Madusanka <madushankaeranda56@gmail.com>
2025-05-02 15:15:57 +0530

    5_2_3

---
+++ blog.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    
+<!-- Mirrored from themebeyond.com/pre/petco-prev/petco-live/blog.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 19 Dec 2024 01:36:20 GMT -->
+<head>
+<?php
+    // Include the header
+    include 'header.php';
+    ?>
+    </head>
+    <body>                             
+        <main>
+
+            <!-- breadcrumb-area -->
+            <section class="breadcrumb-area breadcrumb-bg" data-background="img/bg/breadcrumb_bg.jpg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="breadcrumb-content">
+                                <h2 class="title">Our Blog</h2>
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Our Blog</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- breadcrumb-area-end -->
+
+           
+
+        </main>
+        <!-- main-area-end -->
+
+
+        <!-- footer-start -->
+        <?php
+        // Include the footer at the bottom
+        include 'footer.php';
+        ?>
+        <!-- footer-end -->
+
+
+
+
+
+		<!-- JS here -->
+        <script src="js/vendor/jquery-3.6.0.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/imagesloaded.pkgd.min.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.odometer.min.js"></script>
+        <script src="js/jquery.nice-select.min.js"></script>
+        <script src="js/jquery-ui.min.js"></script>
+        <script src="js/jquery.appear.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+
+<!-- Mirrored from themebeyond.com/pre/petco-prev/petco-live/blog.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 19 Dec 2024 01:36:27 GMT -->
+</html>
